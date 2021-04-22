6649bb816eb324341c77d108cc160ffa43c2622b
jerry0-3 <80954161+jerry0-3@users.noreply.github.com>
2021-04-22 14:42:27 +0200

    Update login_example.py

--- examples/danbooru/login_example.py
+++ examples/danbooru/login_example.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from pybooru import Danbooru
 
-client = Danbooru('danbooru', username='your-username', api_key='yout-api_key')
+client = Danbooru('danbooru', username='your-username', api_key='your-api_key')
 response = client.comment_create(post_id=id, body='your comment')
 
 print(client.last_call)
